54c538d1eb9b9937215a34b5730850e22ca3211e
muyakongali <muyakongali@tencent.com>
2022-09-16 14:22:15 +0800

    starlette application

--- framework_usage.py
+++ framework_usage.py
@@ -1,9 +1,8 @@
-from urllib import response
 from urllib.request import Request
 
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.routing import Route
+from starlette.responses import JSONResponse, PlainTextResponse
+from starlette.routing import Route, WebSocketRoute
 
 
 async def homepage(request: Request) -> JSONResponse:
@@ -18,8 +17,53 @@ async def homepage(request: Request) -> JSONResponse:
     return JSONResponse({'hello': 'world'})
 
 
+async def user_me(request: Request) -> PlainTextResponse:
+    """_summary_
+
+    Args:
+        request (Request): _description_
+
+    Returns:
+        PlainTextResponse: _description_
+    """
+    return PlainTextResponse(f'Hello Archer')
+
+
+async def user(request: Request) -> PlainTextResponse:
+    """_summary_
+
+    Args:
+        request (Request): _description_
+
+    Returns:
+        PlainTextResponse: _description_
+    """
+    return PlainTextResponse(f'Hello {request.path_params["username"]}')
+
+
+async def websocket_endpoint(websocket):
+    """_summary_
+
+    Args:
+        websocket (_type_): _description_
+    """
+    await websocket.accept()
+    await websocket.send_text('Hello websocket')
+    await websocket.close()
+
+
+async def startup() -> None:
+    """_summary_
+    """
+    print('Read to go')
+
+
+routes = [
+    Route('/', homepage),
+    Route('/user/me', user_me),
+    Route('/user/{username}', user),
+    WebSocketRoute('/ws', websocket_endpoint),
+]
 app = Starlette(
-    debug=True, routes=[
-        Route('/', homepage)
-    ]
+    debug=True, routes=routes, on_startup=[startup]
 )
--- tool_usage.py
+++ tool_usage.py
@@ -9,6 +9,9 @@ async def app(scope, receive, send):
         receive (_type_): _description_
         send (_type_): _description_
     """
+    print(f'Debug scope: {scope}')
+    print(f'Debug receive: {receive}')
+    print(f'Debug send: {send}')
     assert scope['type'] == 'http'
     response = PlainTextResponse('Hello World')
     await response(scope, receive, send)
